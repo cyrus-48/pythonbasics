70716355014c9c85b3c562ae9ccf1dc6f83e1530
cyrus mwendwa <cyrusmwendwa370@gmail.com>
2022-11-04 18:47:22 +0300

    new b

--- lab2/linearsearc.py
+++ lab2/linearsearc.py
@@ -5,6 +5,8 @@ def linear_search(list, item):
         if list[i] == item:
             return i
     return None
+# call the function 
 
 my_list = [1, 3, 5, 7, 9]
+print(linear_search(my_list, 3))  # => 1
 print(linear_search(my_list, 3))  # => 1
